9332a7196587c289d31850cd22d0084bb485f2ac
manfortun <m.fortun@arcanys.com>
2024-03-01 15:08:21 +0800

    Add basket controller

---
+++ BrowseBay/Controllers/BasketController.cs
@@ -0,0 +1,20 @@
+﻿using BrowseBay.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrowseBay.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public BasketController(AppDbContext context)
+        {
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
